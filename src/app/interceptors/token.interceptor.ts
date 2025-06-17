f9fc59cfaecce640b0ca89c6a654b4648f148668
sarangasony <sarangasony@gmail.com>
2025-06-17 20:57:30 +0530

    generate interceptor

---
+++ src/app/interceptors/token.interceptor.ts
@@ -0,0 +1,9 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('token');
+  const authReq = token
+    ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
+    : req;
+  return next(authReq);
+};
