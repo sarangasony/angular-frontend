81e01b3741990c6fffb7c4975c00d7149633eec4
sarangasony <sarangasony@gmail.com>
2025-06-17 22:23:15 +0530

    refactor: update app component to use standalone and include RouterModule, enhance app config with HttpClient provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterModule],
+  template: `<router-outlet></router-outlet>`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
